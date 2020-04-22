6d307fbc527e1b6b158f5e57f8aa908754404182
cferdinand <chrisferdinand07@gmail.com>
2020-04-21 20:46:44 -0400

    removed procfile and updated landing page index.js file

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
 
 const port = 4000;
 
