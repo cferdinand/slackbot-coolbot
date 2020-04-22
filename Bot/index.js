318ea4b5455a139adce5f2c67d2fade9320f7295
cferdinand <chrisferdinand07@gmail.com>
2020-04-21 22:49:21 -0400

    index.html

--- Bot/index.js
+++ Bot/index.js
@@ -10,10 +10,10 @@ const bot = new WebClient(token);
     text: "Hey I am coolBot. I can do stuff"
   });
 
-  // setInterval(async () => {
-  //   await bot.chat.postMessage({
-  //     channel: "coolbot-test-sandbox",
-  //     text: "Hey I am coolBot. I post messages at random times"
-  //   });
-  // }, 10000);
+  setInterval(async () => {
+    await bot.chat.postMessage({
+      channel: "coolbot-test-sandbox",
+      text: "Hey I am coolBot. I post messages at random times"
+    });
+  }, 10000);
 })();
